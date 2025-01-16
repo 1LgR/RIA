7f78e04678913c92dc1e8bfbf9062c2d99e1f3b2
Luiz Gustavo Oliveira Rocha <130007640+1LgR@users.noreply.github.com>
2025-01-16 19:17:25 +0000

    :test_tube: test: Criação dos testes para a classe abstrata e suas subclasses

---
+++ subclasses/veiculo.test.ts
@@ -0,0 +1,26 @@
+import { Carro, Moto, Veiculo } from "./veiculo";
+
+describe("Testando Veiculos e método estático", () => {
+    test("Criação e descrição de Carro e Moto", () => {
+        const carro = new Carro("SUV", 2023, "Gasolina");
+        const moto = new Moto("Esportiva", 2022, 600);
+
+        expect(carro.descricao()).toBe("Carro: SUV, Ano: 2023, Combustível: Gasolina");
+        expect(moto.descricao()).toBe("Moto: Esportiva, Ano: 2022, Cilindradas: 600cc");
+    });
+
+    test("Alteração de atributos e comparação de ano", () => {
+        const carro = new Carro("Sedan", 2020, "Flex");
+        const moto = new Moto("Trail", 2020, 250);
+
+        carro.modelo = "Hatch";
+        carro.ano = 2021;
+
+        moto.modelo = "Off-road";
+        moto.ano = 2019;
+
+        const comparacao = Veiculo.compararAno(carro, moto);
+
+        expect(comparacao).toBe("Carro: Hatch, Ano: 2021, Combustível: Flex é mais novo que Moto: Off-road, Ano: 2019, Cilindradas: 250cc");
+    });
+});
